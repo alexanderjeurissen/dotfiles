09dc2b86cb80ae1d799f9112b3638e61fbab0792
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2016-02-06 22:41:12 +0100

    Remove unused old tmux-powerline segments.

--- tmux/segments/rails.sh
+++
@@ -1,9 +0,0 @@
-# Source lib to get the function get_tmux_pwd
-source "${TMUX_POWERLINE_DIR_LIB}/tmux_adapter.sh"
-
-run_segment() {
-   tmux_path=$(get_tmux_cwd)
-
-   cd "$tmux_path"
-   echo $(rails version-name)
-}
