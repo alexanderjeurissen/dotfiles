7d8aebb9f0d40f8b192ff3ff175d24e1e52be5b5
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2016-06-06 01:27:00 +0200

    Add ivy-hydra and ido-ocassional.

--- emacs.d/settings/init-ivy.el
+++ emacs.d/settings/init-ivy.el
@@ -1,3 +1,6 @@
+;; Ivy, Swiper and Ido configuration
+;; Used for fuzzy searching, and file exploration
+
 (use-package smex
   :ensure t
   :config
@@ -6,36 +9,42 @@
 (use-package dash
   :ensure t)
 
-(use-package swiper
-  :Ensure t)
+(use-package ido-occasional
+  :ensure t)
 
 (use-package ivy
   :ensure t
   :config
   (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-height 10)
-  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-use-virtual-buffers t
+        ivy-height 10
+        ivy-count-format "(%d/%d) "
+        ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+
   (setq projectile-completion-system 'ivy)
   (setq magit-completing-read-function 'ivy-completing-read)
-  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy))))
 
-(use-package counsel
-  :ensure t
-  :config
-  (use-package counsel-projectile
-    :ensure t))
+  (use-package counsel
+    :ensure t
+    :config
+    (use-package counsel-projectile ;; Use counsel with projectile
+      :ensure t)
+    (use-package counsel-osx-app ;; use Ivy to launch osx apps
+      :ensure t))
 
-(define-key evil-normal-state-map (kbd "/") 'swiper) ;; I prefer the way swiper searches compared to evil
+  (use-package ivy-hydra
+    :ensure t)
 
-(evil-leader/set-key
-  "b"  'ivy-switch-buffer      ;; Switch to another buffer
-  "f"  'counsel-find-file      ;; find file
-  "/"  'counsel-ag             ;; use ag in none git repositories
+  (global-key-binding (kbd "/") 'swiper)   ;; I prefer the way swiper searches compared to evil
+  (global-key-binding (kbd "C-s") 'swiper) ;; I prefer the way swiper searches compared to I-search
 
-  "pf" 'projectile-find-file   ;; find file in git project
-  "p/" 'counsel-git-grep       ;; use git grep to locate file
+  (evil-leader/set-key
+    "b"  'ivy-switch-buffer      ;; Switch to another buffer
+    "f"  'counsel-find-file      ;; find file
+    "/"  'counsel-ag             ;; use ag in none git repositories
 
-  "x"  'counsel-M-x)
+    "pf" 'projectile-find-file   ;; find file in git project
+    "p/" 'counsel-git-grep       ;; use git grep to locate file
+    "x"  'counsel-M-x))
 
 (provide 'init-ivy)
