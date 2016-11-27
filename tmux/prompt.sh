ffc7f48e285f0b0335157dd7607849036305d98c
Alexander Jeurissen <alexander@jeurissen.email>
2016-11-27 13:01:30 +0100

    Set solarized tmuxt prompt theme as default tmux prompt.

--- tmux/prompt.sh
+++ tmux/prompt.sh
@@ -15,14 +15,10 @@ print_powerline() {
   # If the required PANE_WIDTH isn't met then the segment will be
   # hidden
   if [ $DEBUG_MODE -ne 1 ]; then
-    double_segment "♫" black blue "now_playing" black brightblue 143
-    double_segment "js" black yellow "node" black brightyellow 113
-    double_segment "rb" black red "ruby" black brightred 113
-    double_segment "⎇" black brightred "vcs_branch" black brightred
-    segment "vcs_compare" black black #this is kind of a hack need to refactor
-    double_segment "⊕" black green "vcs_staged" black brightgreen
-    double_segment "+" black yellow "vcs_modified" black brightyellow
-    double_segment "○" black cyan "vcs_others" black brightcyan
+    # double_segment "♫" white blue "now_playing" blue white 143
+    powerline_segment "⬢" green "node" brightgreen black 113
+    powerline_segment "" red "ruby" brightgreen black 113
+    powerline_segment "" black "datetime" brightgreen black 113
   else
     source "${TMUX_POWERLINE_DIR_HOME}/debug_prompt.sh"
   fi
