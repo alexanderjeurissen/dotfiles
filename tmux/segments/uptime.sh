6b4f54713d8533816e3913011555c46348349f1c
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2017-01-08 22:22:22 +0100

    Add new uptime tmux segment.

---
+++ tmux/segments/uptime.sh
@@ -0,0 +1,26 @@
+__process_settings() {
+  case $(uname -s) in
+    *Darwin*)
+      boot=$(sysctl -q -n kern.boottime | awk -F'[ ,:]+' '{ print $4 }')
+      now=$(date +%s)
+      ;;
+    *Linux*|*CYGWIN*)
+      now=$(cut -d' ' -f1 < /proc/uptime)
+      ;;
+    *OpenBSD*)
+      boot=$(sysctl -n kern.boottime)
+      now=$(date +%s)
+  esac
+  export TMUX_POWERLINE_SEG_UPTIME_BOOT=$boot
+  export TMUX_POWERLINE_SEG_UPTIME_NOW=$now
+}
+
+run_segment() {
+    __process_settings
+    uptime=$(expr ${TMUX_POWERLINE_SEG_UPTIME_NOW} - ${TMUX_POWERLINE_SEG_UPTIME_BOOT})
+    d=$(expr $uptime / 86400)
+    h=$(expr $uptime / 3600 % 24)
+    m=$(expr $uptime / 60 % 60)
+    printf " %sd %sh %sm" "$d" "$h" "$m"
+    return 0
+}
