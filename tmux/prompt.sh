c8e822fa158652504602ac6b0a9fbe1e56f5aaff
Alexander Jeurissen <alexanderjeurissen@gmail.com>
2017-01-30 21:08:29 +0100

    Move session_info to separate segment.

--- tmux/prompt.sh
+++ tmux/prompt.sh
@@ -9,16 +9,16 @@ print_powerline() {
   # segment "segment_file_name" foreground background outer_most_segment? min_pane_width
   # If the required PANE_WIDTH isn't met then the segment will be hidden
   if [[ $1 == "left" ]]; then
+    segment "session_info" colour000 colour012 100
     segment "arc_accepted" colour000 colour010 120
     segment "arc_needs_revision" colour000 colour009 120
     segment "arc_needs_review" colour000 colour013 120
     segment "arc_changes_planned" colour000 colour011 120
   else
-    segment "online" colour000 colour009 110
+    # segment "online" colour000 colour009 110
     segment "now_playing" colour000 colour243 140
     # segment "uptime" colour000 colour010 143
     segment "datetime" colour000 colour12 100
-    # segment "hostname" colour000 colour012 143
   fi
 
   exit 0
