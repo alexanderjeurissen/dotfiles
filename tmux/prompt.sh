36c07af998d0d6ccb12da44ea6a9f953b5429d52
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2017-03-13 17:37:53 +0100

    Update tmux statusline to complement cake16 vim colorscheme

--- tmux/prompt.sh
+++ tmux/prompt.sh
@@ -9,17 +9,17 @@ print_powerline() {
   # segment "segment_file_name" foreground background outer_most_segment? min_pane_width
   # If the required PANE_WIDTH isn't met then the segment will be hidden
   if [[ $1 == "left" ]]; then
-    segment "session_info" colour000 colour012 100
-    segment "arc_accepted" colour000 colour010 120
-    segment "arc_needs_revision" colour000 colour009 120
-    segment "arc_needs_review" colour000 colour013 120
-    segment "arc_changes_planned" colour000 colour011 120
+    segment "session_info" colour015 colour01 100
+    segment "arc_accepted" colour014 colour000 120
+    segment "arc_needs_revision" colour014 colour000 120
+    segment "arc_needs_review" colour014 colour000 120
+    segment "arc_changes_planned" colour014 colour000 120
   else
     # segment "online" colour000 colour009 110
     # segment "now_playing" colour000 colour243 140
     # segment "uptime" colour000 colour010 143
     segment "tw_inbox" colour000 colour09
-    segment "datetime" colour000 colour12 100
+    segment "datetime" colour015 colour01 100
   fi
 
   exit 0
