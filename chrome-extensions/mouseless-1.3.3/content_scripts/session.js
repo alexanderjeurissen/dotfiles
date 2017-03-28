1a2f45c48b14012e4d548eeb155a41a71621af68
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2017-03-28 22:03:58 +0200

    Remove mouseless, in favor of vimium.

--- chrome-extensions/mouseless-1.3.3/content_scripts/session.js
+++
@@ -1,9 +0,0 @@
-/**
- * Stores session information that is accessed via mutiple scopes.
- */
-window.Session = {
-  // Accessed for use in indexing document.title
-  ignoreTitleUpdate: false, tabIndex: null,
-
-  isRootFrame: self === top
-};
