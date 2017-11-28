280644a09f9cf429aa0b8927fa3c7e49272444ab
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2017-11-28 22:55:58 +0100

    Remove some unused tmux segments, and add own tmux-pomodoro plugin

--- tmux/segments/tw_inbox.sh
+++
@@ -1,9 +0,0 @@
-# Displays the number of Maniphest tasks
-run_segment() {
-  cd ~/git/hackerone
-  tasks=`arc tasks | egrep -o 'Normal | High | Unbreak Now' | wc -l | tr -d ' '`
-
-  if [[ $tasks > 0 ]]; then
-    echo " TASKS #[bold]${tasks}"
-  fi
-}
