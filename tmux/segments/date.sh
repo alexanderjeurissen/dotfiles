61bb69ffc4731dbec46fcf3b818f4090906e5ffe
Alexander Jeurissen <alexanderjeurissen@gmail.com>
2017-11-28 22:55:58 +0100

    Remove some unused tmux segments, and add own tmux-pomodoro plugin

--- tmux/segments/date.sh
+++
@@ -1,7 +0,0 @@
-# Displays the date in the following format:
-#   DD/MM/YYYY
-
-run_segment() {
-  timeformat="`date +%d/%m/%y`"
-  echo $timeformat
-}
--- tmux/segments/stash_commit.sh
+++
@@ -1,4 +0,0 @@
-# Displays a warning if latest commit is a stash commit
-run_segment() {
-
-}
