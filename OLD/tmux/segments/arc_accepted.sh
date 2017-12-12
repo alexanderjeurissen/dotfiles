b3712e97ca8b262f1d3fc4b263d73017e77a0133
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2017-12-12 23:22:08 +0100

    Add snowblock for tmux related dotfiles and configuration

--- OLD/tmux/segments/arc_accepted.sh
+++
@@ -1,9 +0,0 @@
-# Displays the number of maniphest diffs that are acepted
-run_segment() {
-  cd ~/git/hackerone
-  tasks=`arc list | grep 'Accepted' | wc -l | tr -d ' '`
-
-  if [[ $tasks > 0 ]]; then
-    echo " ${tasks}"
-  fi
-}
--- OLD/tmux/segments/arc_changes_planned.sh
+++
@@ -1,9 +0,0 @@
-# Displays the number of maniphest diffs that have planned changes
-run_segment() {
-  cd ~/git/hackerone
-  tasks=`arc list | grep 'Changes Planned' | wc -l | tr -d ' '`
-
-  if [[ $tasks > 0 ]]; then
-    echo " ${tasks}"
-  fi
-}
