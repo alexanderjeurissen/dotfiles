a006bfc0a2f6f942ad8db3f9fb789b099cf7d4b3
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2018-02-28 00:03:46 +0100

    Experiment with some Ivy settings

--- snowblocks/emacs/settings/init-ivy.el
+++ snowblocks/emacs/settings/init-ivy.el
@@ -14,37 +14,60 @@
 
 (use-package ivy
   :ensure t
+  :diminish ivy-mode
+  :bind (("C-x b" . ivy-switch-buffer)
+         ("C-x B" . ivy-switch-buffer-other-window)
+         ("M-H"   . ivy-resume))
+  :commands (ivy-mode ivy-read ivy-completing-read)
   :config
-  (ivy-mode 1)
   (setq ivy-use-virtual-buffers t
         ivy-height 10
         ivy-count-format "(%d/%d) "
         ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-
+  (ivy-mode 1)
   (setq projectile-completion-system 'ivy)
   (setq magit-completing-read-function 'ivy-completing-read)
 
+
+  (use-package ivy-rich
+    :ensure t
+    :config
+    (ivy-set-display-transformer 'ivy-switch-buffer
+                                 'ivy-rich-switch-buffer-transformer)
+    (setq ivy-virtual-abbreviate 'full
+          ivy-rich-switch-buffer-align-virtual-buffer t)
+    (setq ivy-rich-path-style 'abbrev))
+
+  (use-package swiper
+    :ensure t
+    :bind (("C-s" . swiper)
+           ("C-. C-s" . swiper)
+           ("C-. C-r" . swiper))
+    :commands swiper-from-isearch
+    :init
+    (bind-key "C-." #'swiper-from-isearch isearch-mode-map)
+    :config
+    (bind-key "M-y" #'yank swiper-map)
+    (bind-key "M-%" #'swiper-query-replace swiper-map)
+    (bind-key "M-h" #'swiper-avy swiper-map)
+    (bind-key "M-c" #'swiper-mc swiper-map))
+
   (use-package counsel
     :ensure t
+    :diminish counsel-mode
+    :bind (("M-x"     . counsel-M-x)
+           ("C-h f"   . counsel-describe-function)
+           ("C-h v"   . counsel-describe-variable)
+           ("C-h el" . counsel-find-library)
+           ("C-h eu" . counsel-unicode-char))
     :config
     (use-package counsel-projectile ;; Use counsel with projectile
       :ensure t)
     (use-package counsel-osx-app ;; use Ivy to launch osx apps
-      :ensure t))
+      :ensure t)
+    (counsel-mode 1))
 
   (use-package ivy-hydra
     :ensure t)
-
-  (global-set-key (kbd "C-s") 'swiper) ;; Use swiper instead of I-search
-  (global-set-key (kbd "M-x") 'counsel-M-x) ;; Use Ivy for M-x
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file) ;; Use Ivy for finding files
-  ; (evil-leader/set-key
-  ;   "b"  'ivy-switch-buffer      ;; Switch to another buffer
-  ;   "f"  'counsel-find-file      ;; find file
-  ;   "/"  'counsel-ag             ;; use ag in none git repositories
-
-  ;   "pf" 'projectile-find-file   ;; find file in git project
-  ;   "p/" 'counsel-git-grep       ;; use git grep to locate file
-  ;   "x"  'counsel-M-x))
   )
 (provide 'init-ivy)
