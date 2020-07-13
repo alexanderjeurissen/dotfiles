3b570a9fa34dc5921a3e147c301a1e850d7d7a93
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2020-07-13 12:04:43 +0200

    remove directory aliases

--- config/fish/functions/dotfiles.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function dotfiles --wraps='cd ~/.dotfiles/' --description 'alias dotfiles=cd ~/.dotfiles/'
-  cd ~/.dotfiles/ $argv;
-end
--- config/fish/functions/payments.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function payments --wraps='cd ~/Development/hackerone-payments/' --description 'alias payments=cd ~/Development/hackerone-payments/'
-  cd ~/Development/hackerone-payments/ $argv;
-end
