1f1fcf8e4b1d25113979f5a837f94fd49aa9a175
Alexander Jeurissen <alexander@jeurissen.email>
2021-01-30 18:46:14 +0100

    improve prompt performance by showing cmd duration instead of branch

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -2,7 +2,11 @@ function fish_right_prompt
   # NOTE: exit status segment
   set -l last_status $status
 
-  set_color brgreen; echo (command git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  # NOTE: show git branch
+  # set_color brgreen; echo (command git rev-parse --abbrev-ref HEAD 2>/dev/null)
+
+  # NOTE: show command duration
+  set_color brgreen; echo $CMD_DURATION | humanize_duration
 
   if test $last_status -eq 0
     set_color --bold green; printf "  "
