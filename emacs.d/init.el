927e9d9bd18ccd950cc4aea120d9c3e541c89d26
Alexander Jeurissen <alexander@jeurissen.email>
2024-06-14 16:17:01 +0200

    Update emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,5 @@
-(org-babel-load-file "~/.emacs.d/config.org")
+;; Set vc-follow-symlinks before any checks are made
+(setq vc-follow-symlinks t)
+
+;; Load the rest of the configuration from config.org
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
