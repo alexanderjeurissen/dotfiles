de7c8467e6c1c5b73e307e31d73d17cc5c3a4239
Alexander Jeurissen <alexander@jeurissen.email>
2025-05-26 12:52:41 +0200

    Remove Emacs configuration

--- emacs.d/init.el
+++
@@ -1,5 +0,0 @@
-;; Set vc-follow-symlinks before any checks are made
-(setq vc-follow-symlinks t)
-
-;; Load the rest of the configuration from config.org
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
