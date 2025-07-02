5ab5027896ffd41e873449cafd8a94c13e8fbcc7
Alexander Jeurissen <alexander@jeurissen.email>
2025-07-02 22:11:51 +0200

    migrate to lazy.nvim

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,131 +1,57 @@
-local lspconfig = require('lspconfig')
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local lspconfig = require('lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local map = vim.keymap.set
+      vim.diagnostic.config({
+        virtual_text = {
+          source = "if_many",
+          severity = vim.diagnostic.severity.ERROR,
+          prefix = '⏽',
+        },
+        underline = true,
+        signs = true,
+        severity_sort = true,
+        float = { source = "if_many" },
+      })
+      vim.api.nvim_create_autocmd('CursorHold', { callback = vim.diagnostic.open_float })
+      vim.api.nvim_create_autocmd('CursorHoldI', { callback = vim.diagnostic.open_float })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
 
--- Diagnostics {{{
-
-vim.diagnostic.config({
-  virtual_text = {
-    source = "if_many",  -- Or "if_many"
-    severity = vim.diagnostic.severity.ERROR,
-    prefix = '⏽', -- Could be '■', '▎', 'x'
-  },
-  underline = true,
-  signs = true,
-  severity_sort = true,
-  float = {
-    source = "if_many",  -- Or "if_many"
-  },
-})
-
-vim.api.nvim_create_autocmd('CursorHold', {
-  callback = vim.diagnostic.open_float,
-})
-vim.api.nvim_create_autocmd('CursorHoldI', {
-  callback = vim.diagnostic.open_float,
-})
--- }}}
-
-
--- Signs {{{
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
--- local signs = { Error = "⏽", Warn = "⏽", Hint = "⏽", Info = "⏽" }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
--- }}}
-
---[[ lspconfig.solargraph.setup {
-  capabilities = capabilities,
-  settings = {
-    solargraph = {
-      diagnostics = true,
-      formatting = true,
-      completion = true,
-      definitions = true,
-      symbols = true,
-      hover = true,
-      references = true
-    }
-  }
-} ]]
-
-lspconfig.ruby_lsp.setup {
-  capabilities = capabilities,  -- This is for enabling features like completion, etc.
-  init_options = {
-    enabledFeatures = {
-      codeActions = true,
-      codeLens = true,
-      completion = true,
-      definition = true,
-      diagnostics = true,
-      documentHighlights = true,
-      documentLink = true,
-      documentSymbols = true,
-      foldingRanges = true,
-      formatting = true,
-      hover = true,
-      inlayHint = true,
-      onTypeFormatting = true,
-      selectionRanges = true,
-      semanticHighlighting = true,
-      signatureHelp = true,
-      typeHierarchy = true,
-      workspaceSymbol = true
-    },
- --[[ "featuresConfiguration": {
-      "inlayHint": {
-        "implicitHashValue": true,
-        "implicitRescue": true
+      lspconfig.ruby_lsp.setup { capabilities = capabilities }
+      lspconfig.cssls.setup{ capabilities = capabilities }
+      lspconfig.ts_ls.setup{ capabilities = capabilities }
+      lspconfig.lua_ls.setup{ capabilities = capabilities }
+      lspconfig.jsonls.setup{ capabilities = capabilities }
+      lspconfig.gopls.setup{
+        capabilities = capabilities,
+        cmd = {"gopls", "serve"},
+        settings = {
+          gopls = {
+            analyses = { unusedparams = true },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
       }
-    }, ]]
-    formatter = 'auto',
-    linters = { 'standard', 'rubocop' },
+
+      local map = vim.keymap.set
+      map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { silent = true })
+      map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { silent = true })
+      map('n', 'gh ', '<cmd>lua vim.lsp.buf.hover()<CR>', { silent = true })
+      map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { silent = true })
+      map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { silent = true })
+      map('n', '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>', { silent = true })
+      map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { silent = true })
+      map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { silent = true })
+    end,
   },
 }
-
-lspconfig.cssls.setup{
-  capabilities = capabilities,
-}
-
-lspconfig.ts_ls.setup{ capabilities = capabilities }
-
-lspconfig.lua_ls.setup{
-  capabilities = capabilities,
-}
-
-lspconfig.jsonls.setup{
-  capabilities = capabilities,
-}
-
-lspconfig.gopls.setup{
-  capabilities = capabilities,
-  cmd = {"gopls", "serve"},
-  settings = {
-    gopls = {
-      analyses = {
-	unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true
-    },
-  },
-}
-
-map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { silent = true })
-map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { silent = true })
-map('n', 'gh ', '<cmd>lua vim.lsp.buf.hover()<CR>', { silent = true })
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', { silent = true })
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { silent = true })
-
-map('n', '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>', { silent = true })
-
-map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { silent = true })
-map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { silent = true })
-
--- vim: foldmethod=marker:sw=2:foldlevel=10
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,167 +1,65 @@
--- Telescope requires
-local builtin = require('telescope.builtin')
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
-
-require('telescope').setup{
-  pickers = {
-    -- FIXME: This picker is somehow not recognized so we hardcoded it in the keybinding which is suboptimal
-    marks = {
-      layout_config = { height = 30, width = 100 },
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-zf-native.nvim",
     },
-    projects = {
-      layout_config = { height = 30, width = 100 },
-    },
-    git_files = {
-      layout_config = { height = 30, width = 100 },
-    },
-    find_files = {
-      layout_config = { height = 30, width = 100 },
-    },
-    buffers = {
-      layout_config = { height = 30, width = 100 },
-    },
-    live_grep = {
-      layout_config = { height = 30, width = 200 },
-      preview = { hide_on_startup = false },
-      debounce = 100
-    }
-  },
-  defaults = {
-    preview = {
-      hide_on_startup = true -- hide previewer when picker starts
-    },
-    mappings = {
-      i = {
-        -- close in insert mode
-        ['<esc>'] = actions.close,
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
-        ["<C-a>"] = actions.toggle_all,
-        ["<C-o>"] = actions.send_selected_to_loclist + actions.open_loclist,
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist
-      },
-      n = {
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
-        ["<C-a>"] = actions.toggle_all,
-        ["<C-o>"] = actions.send_selected_to_loclist + actions.open_loclist,
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist
-      }
-    },
-    selection_caret = '❯ ',
-    entry_prefix = '  ',
-    initial_mode = 'insert',
-    selection_strategy = 'reset',
-    sorting_strategy = 'descending',
-  },
-  extensions = {
-    ["zf-native"] = {
-          -- options for sorting file-like items
-          file = {
-              -- override default telescope file sorter
-              enable = true,
-
-              -- highlight matching text in results
-              highlight_results = true,
-
-              -- enable zf filename match priority
-              match_filename = true,
-
-              -- optional function to define a sort order when the query is empty
-              initial_sort = nil,
-
-              -- set to false to enable case sensitive matching
-              smart_case = true,
+    config = function()
+      local actions = require('telescope.actions')
+      require('telescope').setup{
+        pickers = {
+          marks = { layout_config = { height = 30, width = 100 } },
+          projects = { layout_config = { height = 30, width = 100 } },
+          git_files = { layout_config = { height = 30, width = 100 } },
+          find_files = { layout_config = { height = 30, width = 100 } },
+          buffers = { layout_config = { height = 30, width = 100 } },
+          live_grep = {
+            layout_config = { height = 30, width = 200 },
+            preview = { hide_on_startup = false },
+            debounce = 100,
           },
-
-          -- options for sorting all other items
-          generic = {
-              -- override default telescope generic item sorter
-              enable = true,
-
-              -- highlight matching text in results
-              highlight_results = true,
-
-              -- disable zf filename match priority
-              match_filename = false,
-
-              -- optional function to define a sort order when the query is empty
-              initial_sort = nil,
-
-              -- set to false to enable case sensitive matching
-              smart_case = true,
+        },
+        defaults = {
+          preview = { hide_on_startup = true },
+          mappings = {
+            i = {
+              ['<esc>'] = actions.close,
+              ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
+              ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
+              ["<C-a>"] = actions.toggle_all,
+              ["<C-o>"] = actions.send_selected_to_loclist + actions.open_loclist,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+            n = {
+              ["<Tab>"] = actions.toggle_selection + actions.move_selection_next,
+              ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_previous,
+              ["<C-a>"] = actions.toggle_all,
+              ["<C-o>"] = actions.send_selected_to_loclist + actions.open_loclist,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
           },
+          selection_caret = '❯ ',
+          entry_prefix = '  ',
+          initial_mode = 'insert',
+          selection_strategy = 'descending',
+        },
+        extensions = {
+          ["zf-native"] = { file = { enable = true, highlight_results = true, match_filename = true, smart_case = true },
+                            generic = { enable = true, highlight_results = true, match_filename = false, smart_case = true } },
+        },
       }
-  }
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>m', builtin.marks, { noremap = true, silent = true, desc = 'List marks' })
+      vim.keymap.set('n', '<leader>tl', ":Telescope<CR>", { noremap = true, silent = true, desc = 'Open Telescope' })
+      vim.keymap.set('n', '<leader>pf', builtin.git_files, { noremap = true, silent = true, desc = 'Project files' })
+      vim.keymap.set('n', '<leader>p/', builtin.live_grep, { noremap = true, silent = true, desc = 'Grep project' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { noremap = true, silent = true, desc = 'Find files' })
+      vim.keymap.set('n', '<leader>bb', builtin.buffers, { noremap = true, silent = true, desc = 'List buffers' })
+      vim.keymap.set('n', 'gb', builtin.buffers, { noremap = true, silent = true, desc = 'List buffers' })
+      vim.keymap.set('n', '<leader>gF', builtin.git_files, { noremap = true, silent = true, desc = 'Git files' })
+      vim.keymap.set('n', '<leader>gf', builtin.git_status, { noremap = true, silent = true, desc = 'Git status' })
+      require('telescope').load_extension('zf-native')
+    end,
+  },
 }
-
--- Files bindings {{{
--- Open general Telescope picker
-vim.keymap.set('n', '<leader>m', "<cmd>lua require('telescope.builtin').marks()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'List marks'
-})
-
--- Open general Telescope picker
-vim.keymap.set('n', '<leader>tl', "<cmd>Telescope<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Open Telescope'
-})
-
--- Find files in the current project (similar to FZF's project_files)
-vim.keymap.set('n', '<leader>pf', "<cmd>lua require('telescope.builtin').git_files()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Project files'
-})
-
--- Live grep in the project
-vim.keymap.set('n', '<leader>p/', "<cmd>lua require('telescope.builtin').live_grep()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Grep project'
-})
-
--- Find files using the file browser
-vim.keymap.set('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Find files'
-})
--- }}}
-
--- Buffer bindings {{{
--- List open buffers
-vim.keymap.set('n', '<leader>bb', "<cmd>lua require('telescope.builtin').buffers()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'List buffers'
-})
-vim.keymap.set('n', 'gb', "<cmd>lua require('telescope.builtin').buffers()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'List buffers'
-})
--- }}}
-
--- Git bindings {{{
--- List files changed in current branch
-vim.keymap.set('n', '<leader>gF', "<cmd>lua require('telescope.builtin').git_files()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Git files'
-})
-
--- List git diffs for current file
-vim.keymap.set('n', '<leader>gf', "<cmd>lua require('telescope.builtin').git_status()<CR>", {
-  noremap = true,
-  silent = true,
-  desc = 'Git status'
-})
--- }}}
-
--- Load Telescope extensions
-require('telescope').load_extension('zf-native')
